f02dc48ec3226b5421412ae9c0f48033336841c0
tobiascamilo <tobiascamylo@gmail.com>
2024-08-02 17:22:29 -0300

    creates and configures the homePage class

---
+++ cypress/support/pages/home/elements.js
@@ -0,0 +1,7 @@
+export const ELEMENTS = {
+    home_title: '.title',
+    item_container: '.inventory_item',
+    add_to_cart_button: '#add-to-cart-sauce-labs-backpack',
+    cart_badge: '.shopping_cart_badge',
+    remove_from_cart: '#remove-sauce-labs-backpack'
+}
---
+++ cypress/support/pages/home/homePage.js
@@ -0,0 +1,30 @@
+/// <reference types="cypress" />
+
+const elem = require('../../pages/home/elements').ELEMENTS
+
+class home{
+
+    homePageIsVisible(){
+        cy.get(elem.home_title).should('have.text', 'Products')
+        //cy.get(elem.cart).should('to.exist')
+    }
+    
+    homePageIsNotVisible(){
+        cy.get(elem.home_title).should('not.to.exist')
+    }
+
+    productsAreVisible(){
+        cy.get(elem.item_container).should('be.visible')
+    }
+
+    addItemToCart(){
+        cy.get(elem.add_to_cart_button).click()
+    }
+
+    showCartBadge(){
+        cy.get(elem.cart_badge).should('be.visible')
+    }
+
+}
+
+export default new home()
