c7de65e0ce1581bd1eea1332cbc07b1c05cc06ec
tobiascamilo <tobiascamylo@gmail.com>
2024-08-02 14:46:39 -0300

    creates login page elements object that maps login page elements

---
+++ cypress/support/pages/login/elements.js
@@ -0,0 +1,7 @@
+export const ELEMENTS = {
+    username: '#user-name',
+    password: '#password',
+    loginButton: '#login-button',
+    home_menu: '#react-burger-menu-btn',
+    logoff_button: '#logout_sidebar_link'
+}
