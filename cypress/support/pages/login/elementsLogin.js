06ff6b259b9a330281e5b246e7f6a3d2426ffdb9
tobiascamilo <tobiascamylo@gmail.com>
2024-08-03 11:48:30 -0300

    refactors the test login

--- cypress/support/pages/login/elementsLogin.js
+++ cypress/support/pages/login/elementsLogin.js
@@ -5,11 +5,3 @@ export const ELEMENTS = {
     home_menu: '#react-burger-menu-btn',
     logoff_button: '#logout_sidebar_link'
 }
-
-// export const ELEMENTS = {
-//     username: '#user-name',
-//     password: '#password',
-//     loginButton: '[data-test="login-button"]',
-//     home_menu: '#react-burger-menu-btn',
-//     logoff_button: '#logout_sidebar_link'
-// }
